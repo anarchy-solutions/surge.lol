e0789cbec1d78a189994afcbca041d5542d3bb77
anarchy-solutions <164175212+anarchy-solutions@users.noreply.github.com>
2024-08-09 01:51:54 +0200

    Update loader-streamables.lua

--- loader-streamables.lua
+++ loader-streamables.lua
@@ -1,11 +1,11 @@
 -- [[ arg table, i hope i dont fuck this up 💔]] --
-local games = {
+local games2 = {
     [1] = XXXXXXXXXXXXXX,
     [2] = XXXXXXXXXXXXXXXX,
     [3] = XXXXXXXXXXXXX,
 }
 
-for _, placeId in pairs(games) do
+for _, placeId in pairs(games2) do
     if game.PlaceId == placeId then
         print("your current game is on the streamables list:" .. currentPlaceId)
         break
