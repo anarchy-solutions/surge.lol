96d9905c3c3023f45d84ee47035a83bec23c2159
anarchy-solutions <164175212+anarchy-solutions@users.noreply.github.com>
2024-08-10 18:12:34 +0200

    Update and rename source.lua to mainloader.lua

---
+++ mainloader.lua
@@ -0,0 +1,28 @@
+--[[
+$$$$$$$\  $$\        $$$$$$\  $$\      $$\ $$$$$$$$\     $$\      $$\ $$$$$$$$\ $$$$$$$$\ 
+$$  __$$\ $$ |      $$  __$$\ $$$\    $$$ |$$  _____|    $$ | $\  $$ |\__$$  __|$$  _____|
+$$ |  $$ |$$ |      $$ /  $$ |$$$$\  $$$$ |$$ |          $$ |$$$\ $$ |   $$ |   $$ |      
+$$$$$$$\ |$$ |      $$$$$$$$ |$$\$$\$$ $$ |$$$$$\        $$ $$ $$\$$ |   $$ |   $$$$$\    
+$$  __$$\ $$ |      $$  __$$ |$$ \$$$  $$ |$$  __|       $$$$  _$$$$ |   $$ |   $$  __|   
+$$ |  $$ |$$ |      $$ |  $$ |$$ |\$  /$$ |$$ |          $$$  / \$$$ |   $$ |   $$ |      
+$$$$$$$  |$$$$$$$$\ $$ |  $$ |$$ | \_/ $$ |$$$$$$$$\ $$\ $$  /   \$$ |   $$ |   $$ |      
+\_______/ \________|\__|  \__|\__|     \__|\________|\__|\__/     \__|   \__|   \__|      
+                                                                                          
+                                                                                                  
+ THIS SCRIPT IS UNDER THE LICENSE OF MoonX Softworks. Refrain from reverse engineering, modifying or claiming the ownership of this product!
+--]]
+
+local supported = {
+    [1] = 2788229376,
+    [2] = 10101010101,
+    [3] = 111111111,
+}
+
+for _, placeId in pairs(games2) do
+    if game.PlaceId == placeId then
+        print("your current game is on the supported games' list!")
+        task.wait(.5)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/anarchy-solutions/-blame.wtf/main/main.lua"))()
+        break
+    end
+end
