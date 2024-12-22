2d7e0fbe183868774491436cc54422f6e3a68fb7
anarchy-solutions <164175212+anarchy-solutions@users.noreply.github.com>
2024-12-23 00:25:17 +0100

    Update mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -1,29 +1,11 @@
---[[
-$$$$$$$\  $$\        $$$$$$\  $$\      $$\ $$$$$$$$\     $$\      $$\ $$$$$$$$\ $$$$$$$$\ 
-$$  __$$\ $$ |      $$  __$$\ $$$\    $$$ |$$  _____|    $$ | $\  $$ |\__$$  __|$$  _____|
-$$ |  $$ |$$ |      $$ /  $$ |$$$$\  $$$$ |$$ |          $$ |$$$\ $$ |   $$ |   $$ |      
-$$$$$$$\ |$$ |      $$$$$$$$ |$$\$$\$$ $$ |$$$$$\        $$ $$ $$\$$ |   $$ |   $$$$$\    
-$$  __$$\ $$ |      $$  __$$ |$$ \$$$  $$ |$$  __|       $$$$  _$$$$ |   $$ |   $$  __|   
-$$ |  $$ |$$ |      $$ |  $$ |$$ |\$  /$$ |$$ |          $$$  / \$$$ |   $$ |   $$ |      
-$$$$$$$  |$$$$$$$$\ $$ |  $$ |$$ | \_/ $$ |$$$$$$$$\ $$\ $$  /   \$$ |   $$ |   $$ |      
-\_______/ \________|\__|  \__|\__|     \__|\________|\__|\__/     \__|   \__|   \__|      
-                                                                                          
-                                                                                                  
+--[[                                                                                             
  THIS SCRIPT IS UNDER THE LICENSE OF MoonX Softworks. Refrain from reverse engineering, modifying or claiming the ownership of this product!
 --]]
 
-supported = {
+supported = { -- this is now useless, script will execute even if the game is not listed in the table
     [1] = 2788229376,
     [2] = 301549746,
     [3] = 111111111,
 }
 
-for _, placeId in pairs(supported) do
-    if game.PlaceId == placeId then
-        print("your current game is on the supported games' list!")
-        task.wait(.5)
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/anarchy-solutions/-blame.wtf/main/main.lua"))()
-        break
-    end
-end
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/anarchy-solutions/-blame.wtf/main/main.lua"))()
